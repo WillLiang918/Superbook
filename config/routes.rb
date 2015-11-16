f6eb322001c2e7a190f06a3f4b6cef4e9e5ce5ce
App Academy Student <ryandgoldenberg1@gmail.com>
2015-11-16 15:31:41 -0500

    Add sign up page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'login' => 'sessions#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
