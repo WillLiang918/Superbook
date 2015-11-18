b7e4774971f0438de1126f639795ed5d3e7421ed
App Academy Student <ryandgoldenberg1@gmail.com>
2015-11-18 09:48:57 -0500

    Add link to author name.

--- app/assets/javascripts/components/posts/author.js.jsx
+++ app/assets/javascripts/components/posts/author.js.jsx
@@ -3,12 +3,12 @@ var Author = React.createClass({
     var post = this.props.post;
     var author = post.author;
     var authorName = author.first_name + " " + author.last_name;
-    
+
     return (
       <section className="author-details flex-container">
         <div className="thumbnail" />
         <div>
-          <a href="#" className="user-link">{authorName}</a>
+          <Link to={"users/" + author.id} className="user-link">{authorName}</Link>
           <a href="#" className="post-link">{post.updated_at}</a>
         </div>
       </section>
