ed42e00f2e8e84486839a6788585bccb414b3bbc
App Academy Student <ryandgoldenberg1@gmail.com>
2015-11-20 11:37:41 -0500

    Validate uniqueness of friendship.

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,6 +3,7 @@ class Friendship < ActiveRecord::Base
   belongs_to :friend, class_name: "User", foreign_key: :friend_id
 
   validates :user_id, :friend_id, presence: true
+  validates :user_id, uniqueness: { scope: :friend_id }
   validate :cannot_friend_yourself
 
   def cannot_friend_yourself
