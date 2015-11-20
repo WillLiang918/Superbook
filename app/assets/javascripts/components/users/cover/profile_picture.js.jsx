ec402457454d5dc857af8e1889d737a61d0c82da
App Academy Student <ryandgoldenberg1@gmail.com>
2015-11-20 17:49:09 -0500

    Display profile picture on UserPage.

--- app/assets/javascripts/components/users/cover/profile_picture.js.jsx
+++ app/assets/javascripts/components/users/cover/profile_picture.js.jsx
@@ -1,7 +1,7 @@
 var ProfilePicture = React.createClass({
   render: function() {
     return (
-      <div className="profile-picture" />
+      <img src={this.props.url} className="profile-picture" />
     );
   }
 });
--- app/assets/javascripts/components/users/cover/users_cover.js.jsx
+++ app/assets/javascripts/components/users/cover/users_cover.js.jsx
@@ -1,10 +1,15 @@
 var UserCover = React.createClass({
   render: function() {
+    var user = this.props.user, url = "";
+    if (!!user.avatar) {
+      url = user.avatar.profile;
+    }
+
     return (
       <section className="users-cover">
         <CoverPhoto />
-        <ProfilePicture />
-        <UserTitle user={this.props.user} />
+        <ProfilePicture url={url} />
+        <UserTitle user={user} />
         <UserFriendNav {...this.props} />
         <UserPageNav />
       </section>
