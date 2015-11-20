598b8b59de41a90835a57aee7f91e0a62c4748bb
App Academy Student <ryandgoldenberg1@gmail.com>
2015-11-20 12:14:12 -0500

    Create Frienship Store and action for accepting Friend Requests.

---
+++ app/assets/javascripts/stores/friendship_store.js
@@ -0,0 +1,35 @@
+(function(root) {
+  var _friendships = {};
+
+  var addFriendship = function(friendship) {
+    var user_id = friendship.user_id, friend_id = friendship.friend_id;
+    _friendships[user_id] = _friendships[user_id] || new Set();
+    _friendships[friend_id] = _friendships[friend_id] || new Set();
+    _friendships[user_id].add(friend_id);
+    _friendships[friend_id].add(user_id);
+  };
+
+  var acceptFriendRequest = function(request) {
+    var friendship = {user_id: request.sender_id, friend_id: request.friend_id};
+    addFriendship(friendship);
+  };
+
+  root.FriendshipStore = Object.assign({}, root.StoreBase, {
+
+    find: function(userId) {
+      return _friendships[userId] || new Set();
+    },
+
+    dispatcherId: AppDispatcher.register(function(payload) {
+      switch(payload.actionType) {
+
+        case Constants.FRIEND_REQUEST_ACCEPTED:
+          acceptFriendRequest(payload.request);
+          root.FriendshipStore.emitChange();
+          break;
+
+      }
+    })
+
+  });
+})(this);
