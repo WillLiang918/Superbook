9eba909d10f27c537bbd389e3381bec5bd14120d
App Academy Student <ryandgoldenberg1@gmail.com>
2015-11-20 11:16:06 -0500

    Create Friendship model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118181831) do
+ActiveRecord::Schema.define(version: 20151120161205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20151118181831) do
 
   add_index "friend_requests", ["receiver_id"], name: "index_friend_requests_on_receiver_id", using: :btree
   add_index "friend_requests", ["sender_id"], name: "index_friend_requests_on_sender_id", using: :btree
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "friend_id",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "friendships", ["user_id"], name: "index_friendships_on_user_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "body",        null: false
