90a073fe443f5cf326ca93869e285371b85a2354
Ryan Goldenberg <ryandgoldenberg1@gmail.com>
2015-11-19 21:02:44 -0500

    Complete styles for hover button.

--- app/assets/javascripts/components/users/friend_requests/answer_friend_request.js.jsx
+++ app/assets/javascripts/components/users/friend_requests/answer_friend_request.js.jsx
@@ -5,8 +5,8 @@ var AnswerFriendRequest = React.createClass({
     return (
       <p className="answer-friend-request">
         <strong>{user.first_name + " " + user.last_name}</strong> sent you a friend request
-        <button className="blue hover-bubble" data-hover-text="Confirm Request">Confirm Request</button>
-        <button className="gray hover-bubble" data-hover-text="Delete Request">Delete Request</button>
+        <button className="blue hover-bubble" data-hover="Confirm Request">Confirm Request</button>
+        <button className="gray hover-bubble" data-hover="Delete Request">Delete Request</button>
       </p>
     );
   }
