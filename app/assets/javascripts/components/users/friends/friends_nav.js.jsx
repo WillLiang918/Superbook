ed92d2a50e8edfba8b2b0dd316ba88e02c749d1d
App Academy Student <ryandgoldenberg1@gmail.com>
2015-11-21 18:28:44 -0500

    Style friend search.

--- app/assets/javascripts/components/users/friends/friends_nav.js.jsx
+++ app/assets/javascripts/components/users/friends/friends_nav.js.jsx
@@ -6,13 +6,16 @@ var FriendsNav = React.createClass({
           <a href="#">All Friends</a>
         </div>
 
-        <input
-          type="text"
-          className="search-friends"
-          value={this.props.search}
-          placeholder="Search Friends"
-          onChange={this.props._onChange}
-        />
+        <div className="search-friends-container">
+          <input
+            type="text"
+            className="search-friends"
+            value={this.props.search}
+            placeholder="Search Friends"
+            onChange={this.props._onChange}
+          />
+          <strong className="small-magnifying-glass" />
+        </div>
       </nav>
     );
   }
