245e67748b7b0092d1ffe7f8d642006b0121b733
App Academy Student <ryandgoldenberg1@gmail.com>
2015-11-22 15:49:10 -0500

    Add validation for parent comment.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,19 @@
 class Comment < ActiveRecord::Base
+  belongs_to :commentable, polymorphic: true
+  belongs_to :parent, class_name: "Comment", foreign_key: :parent_id
+  belongs_to :author, class_name: "User", foreign_key: :author_id
+
+  validates :author, :commentable, :body, presence: true
+  validate :valid_parent_comment
+
+  def valid_parent_comment
+    return if self.parent_id.nil?
+
+    parent = self.parent
+    unless parent &&
+           parent.commentable_id == self.commentable_id &&
+           parent.commentable_type == self.commentable_type
+      errors.add(:parent, "must point to the same commentable")
+    end
+  end
 end
--- db/migrate/20151122202721_create_comments.rb
+++ db/migrate/20151122202721_create_comments.rb
@@ -4,7 +4,7 @@ class CreateComments < ActiveRecord::Migration
       t.integer :author_id, null: false, index: true, foreign_key: true
       t.references :commentable, polymorphic: true, index: true, null: false
       t.string :body, null: false
-      t.integer :parent_id, index: true
+      t.integer :parent_id, index: true 
 
       t.timestamps null: false
     end
