45775ef0c30c17c2f1559c9bc53c8ad6422fb4b9
App Academy Student <ryandgoldenberg1@gmail.com>
2015-11-22 13:21:46 -0500

    Complete friend request drop down menu.

--- app/assets/javascripts/components/layout/header_icon_links.js.jsx
+++ app/assets/javascripts/components/layout/header_icon_links.js.jsx
@@ -1,33 +1,64 @@
 var HeaderIconLinks = React.createClass({
   getInitialState: function() {
-    return {};
+    return {activeIdx: -1};
   },
-  componentWillMount: function() {
-
+  handleDOMClick: function(e) {
+    var $target = $(event.target);
+    var isInsideContainer = $target.is("#header-icon-links *, #header-icon-links");
+    if (!isInsideContainer) {
+      this.setState({activeIdx: -1});
+    }
   },
   componentDidMount: function() {
-
+    $(document).on("click", this.handleDOMClick);
   },
   componentWillUnmount: function() {
-
+    if (typeof this.clickHandler === "function") {
+      document.off("click", this.handleDOMClick);
+    }
+  },
+  changeActiveIdx: function(e) {
+    var idx = parseInt(e.currentTarget.dataset.idx);
+    if (idx === this.state.activeIdx) { idx = -1; }
+    this.setState({activeIdx: idx});
   },
   render: function() {
+    var tabIcons = [<button className="friend-requests-icon" />];
+    var tabContent = [
+      (
+        <div>
+          <header className="content-tab-header">
+            <h3>Friend Requests</h3>
+          </header>
+
+          <FriendRequestList requesters={this.props.requesters} />
+        </div>
+      )
+    ];
+    var activeIdx = this.state.activeIdx;
+
     return (
-      <div className="header-icon-links">
+      <div className="header-icon-links" id="header-icon-links">
         <ul className="icon-link-list flex-container-start">
-          <li className="active">
-            <button className="friend-requests-icon" />
-          </li>
+          {tabIcons.map(function(icon, idx) {
+            var className = (activeIdx === idx ? "active" : "");
+
+            return (
+              <li className={className} key={idx} data-idx={idx} onClick={this.changeActiveIdx}>
+                {icon}
+              </li>
+            );
+          }, this)}
         </ul>
 
         <section className="header-icon-content">
-          <article className="content-tab active">
-            <header className="content-tab-header">
-              <h3>Header</h3>
-            </header>
-
-            <FriendRequestList requesters={this.props.requesters} />
-          </article>
+          {tabContent.map(function(content, idx) {
+            return (
+              <article className={"content-tab " + (activeIdx === idx ? "active" : "")} key={idx}>
+                {content}
+              </article>
+            );
+          }, this)}
         </section>
       </div>
     );
