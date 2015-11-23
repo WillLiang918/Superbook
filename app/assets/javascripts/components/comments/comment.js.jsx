df785ee191e125e308b1033c29bd2f20c00c7009
App Academy Student <ryandgoldenberg1@gmail.com>
2015-11-22 21:59:57 -0500

    Style comment body and author link.

--- app/assets/javascripts/components/comments/comment.js.jsx
+++ app/assets/javascripts/components/comments/comment.js.jsx
@@ -3,11 +3,16 @@ var Comment = React.createClass({
     var {comment, users, ...other} = this.props;
     var user = users[comment.author_id];
     var avatarUrl = user.avatar.profile;
+    var userUrl = "/users/" + user.id;
+    var userName = user.first_name + " " + user.last_name;
 
     return (
       <article className="comment flex-container">
         <Thumbnail user={user} />
-        <p className="comment-body">{comment.body}</p>
+        <p className="comment-body">
+          <Link to={userUrl} className="author-name">{userName}</Link>
+          {comment.body}
+        </p>
       </article>
     );
   }
