f1231206d19beef9cd6ce5a8c8545d3427489fb7
App Academy Student <ryandgoldenberg1@gmail.com>
2015-11-23 13:19:57 -0500

    Add before action to CommentsController to verify destroy permission.

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,5 +1,7 @@
 class Api::CommentsController < ApplicationController
 
+  before_action :must_have_permission_to_destroy, only: [:destroy]
+
   def create
     @comment = Comment.new(comment_params)
     @comment.author = current_user
@@ -17,6 +19,15 @@ class Api::CommentsController < ApplicationController
 
     def comment_params
       params.require(:comment).permit(:commentable_type, :commentable_id, :body, :parent_id)
+    end
+
+    def must_have_permission_to_destroy
+      return unless params[:commentable_type] == "Post"
+      return if current_user.id == params[:author_id].to_i
+      post = Post.find(params[:commentable_id])
+      if current_user.id != post.sender_id && current_user.id != post.receiver_id
+        render text: "You do not have permission to delete comment", status: :unauthorized
+      end
     end
 
 end
