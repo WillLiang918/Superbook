a3644bc34835a2f3a39632920013c18f1e1f7e9b
App Academy Student <ryandgoldenberg1@gmail.com>
2015-11-25 15:27:03 -0500

    Blink comment box on comment link click.

--- app/assets/javascripts/components/posts/post_action_nav.js.jsx
+++ app/assets/javascripts/components/posts/post_action_nav.js.jsx
@@ -2,12 +2,12 @@ var PostActionNav = React.createClass({
   render: function() {
     var liked = this.props.liked;
     var className = "post-like-action" + (liked ? " liked" : "");
-    var onClick = (liked ? this.unlike : this.like);
+    var onLike = (liked ? this.unlike : this.like);
 
     return (
       <div className="post-action-nav">
-        <a className={className} onClick={onClick}>Like</a>
-        <a className="post-comment-action">Comment</a>
+        <a className={className} onClick={onLike}>Like</a>
+        <a className="post-comment-action" onClick={this.goToComment}>Comment</a>
       </div>
     );
   },
@@ -20,5 +20,17 @@ var PostActionNav = React.createClass({
     var post = this.props.post;
     var data = {likeable_id: post.id, likeable_type: "Post"};
     ApiUtil.unlike(data);
+  },
+  goToComment: function() {
+    var $this = $(ReactDOM.findDOMNode(this));
+    var $li = $this.closest("li");
+    var $form = $li.find(".comment-box > .comment-form");
+    var $input = $form.find("input[type='text']");
+
+    $input.focus().css({ backgroundColor: "#F9FDC8" });
+
+    setTimeout(function() {
+      $input.animate({ backgroundColor: "#fff" }, 200);
+    }, 0);
   }
 });
