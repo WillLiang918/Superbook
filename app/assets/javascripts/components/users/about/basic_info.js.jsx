5a850e164e7c1f61f61487b496e918c58fa1c1ed
App Academy Student <ryandgoldenberg1@gmail.com>
2015-11-26 17:12:37 -0500

    Style user's about page.

--- app/assets/javascripts/components/users/about/basic_info.js.jsx
+++ app/assets/javascripts/components/users/about/basic_info.js.jsx
@@ -1,7 +1,35 @@
 var BasicInfo = React.createClass({
   render: function() {
     return (
-      false
+      <article className="user-basic-info">
+        <h4>Basic Information</h4>
+
+        <ul>
+          <li>
+            <div>Birth Date</div>
+            <div>Jan 1 1999</div>
+          </li>
+
+          <li>
+            <div>Sex</div>
+            <div>Male</div>
+          </li>
+
+          <li>
+            <a className="field">
+              <strong className="plus-icon" />
+              <span>Add your workplace</span>
+            </a>
+          </li>
+
+          <li>
+            <a className="field">
+              <strong className="plus-icon" />
+              <span>Add your hometown</span>
+            </a>
+          </li>
+        </ul>
+      </article>
     );
   }
 });
--- app/assets/javascripts/components/users/about/contact.js.jsx
+++ app/assets/javascripts/components/users/about/contact.js.jsx
@@ -1,7 +1,30 @@
 var Contact = React.createClass({
   render: function() {
     return (
-      false
+      <article className="user-contact">
+        <h4>Contact Information</h4>
+
+        <ul>
+          <li>
+            <div>Email</div>
+            <div>user@gmail.com</div>
+          </li>
+
+          <li>
+            <a className="field">
+              <strong className="plus-icon" />
+              <span>Add a mobile phone</span>
+            </a>
+          </li>
+
+          <li>
+            <a className="field">
+              <strong className="plus-icon" />
+              <span>Add your address</span>
+            </a>
+          </li>
+        </ul>
+      </article>
     );
   }
 });
