d66164e55785c7fba65cd2e410ea6bcb9611a57a
App Academy Student <ryandgoldenberg1@gmail.com>
2015-11-26 17:19:44 -0500

    add validations and associations for nicknames.

--- app/models/nickname.rb
+++ app/models/nickname.rb
@@ -1,2 +1,5 @@
 class Nickname < ActiveRecord::Base
+  belongs_to :profile
+
+  validates :profile, :nickname, presence: true
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,6 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  has_many :nicknames, dependent: :destroy
 
   validates :user, presence: true
 end
