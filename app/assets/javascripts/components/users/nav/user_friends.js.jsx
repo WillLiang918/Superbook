8cd69c7239bcd0870564827c2052499c04c8ab8f
App Academy Student <ryandgoldenberg1@gmail.com>
2015-11-26 02:41:42 -0500

    Only show friends in side nav if user has at least 1.

--- app/assets/javascripts/components/users/nav/user_friends.js.jsx
+++ app/assets/javascripts/components/users/nav/user_friends.js.jsx
@@ -1,6 +1,7 @@
 var UserFriends = React.createClass({
   render: function() {
     var friends = this.props.friends;
+    if (friends.length === 0) { return false; }
 
     return (
       <article className="user-friends">
