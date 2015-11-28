3aee8b4a1efe2164547c63fba8d98d3a86102958
Ryan Goldenberg <ryandgoldenberg1@gmail.com>
2015-11-28 12:30:55 -0500

    Fix styling for demo users and render nothing in demo user controller.

--- app/controllers/api/demo_users_controller.rb
+++ app/controllers/api/demo_users_controller.rb
@@ -2,10 +2,12 @@ class Api::DemoUsersController < ApplicationController
 
   def add
     DemoUsers.add(params[:name])
+    render nothing: true
   end
 
   def remove
     DemoUsers.remove(params[:name])
+    render nothing: true
   end
 
 end
