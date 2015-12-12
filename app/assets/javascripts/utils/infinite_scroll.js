a23640d0fbe104f75c9f05dfcd1e6f1d3847e0b1
App Academy Student <ryandgoldenberg1@gmail.com>
2015-12-11 19:50:36 -0500

    Make scroll ratio constant inside InfiniteScroll mixin.

--- app/assets/javascripts/utils/infinite_scroll.js
+++ app/assets/javascripts/utils/infinite_scroll.js
@@ -1,49 +1,45 @@
-(function(root) {
+var InfiniteScroll = {
+  _SCROLL_RATIO: 0.75,
 
-  var SCROLL_RATIO = 0.75;
+  componentDidMount: function() {
+    this._fetchInitialData();
+    $(window).on("scroll", this._onScroll);
+  },
 
-  window.InfiniteScroll = {
-    componentDidMount: function() {
-      this._fetchInitialData();
-      $(window).on("scroll", this._onScroll);
-    },
+  componentWillUnmount: function() {
+    $(window).off("scroll", this._onScroll);
+  },
 
-    componentWillUnmount: function() {
-      $(window).off("scroll", this._onScroll);
-    },
+  _onScroll: function(e) {
+    var maxScroll = $(document).height() - $(window).height();
+    var percentage = window.scrollY / maxScroll;
 
-    _onScroll: function(e) {
-      var maxScroll = $(document).height() - $(window).height();
-      var percentage = window.scrollY / maxScroll;
+    if (percentage >= this._SCROLL_RATIO)
+      this._fetchMoreData();
+  },
 
-      if (percentage >= SCROLL_RATIO)
-        this._fetchMoreData();
-    },
+  _fetchInitialData: function() {
+    if (typeof this.fetchInitialData !== "function")
+      throw "InfiniteScroll requires fetchInitialData()";
 
-    _fetchInitialData: function() {
-      if (typeof this.fetchInitialData !== "function")
-        throw "InfiniteScroll requires fetchInitialData()";
+    this._startFetching();
+    this.fetchInitialData().then(this._finishFetching);
+  },
 
-      this._startFetching();
-      this.fetchInitialData().then(this._finishFetching);
-    },
+  _fetchMoreData: function() {
+    if (typeof this.fetchMoreData !== "function")
+      throw "InfiniteScroll requires fetchMoreData()";
 
-    _fetchMoreData: function() {
-      if (typeof this.fetchMoreData !== "function")
-        throw "InfiniteScroll requires fetchMoreData()";
+    if (this._isFetching) return;
+    this._startFetching();
+    this.fetchMoreData().then(this._finishFetching);
+  },
 
-      if (this._isFetching) return;
-      this._startFetching();
-      this.fetchMoreData().then(this._finishFetching);
-    },
+  _startFetching: function() {
+    this._isFetching = true;
+  },
 
-    _startFetching: function() {
-      this._isFetching = true;
-    },
-
-    _finishFetching: function() {
-      this._isFetching = false;
-    }
-  };
-  
-})(this);
+  _finishFetching: function() {
+    this._isFetching = false;
+  }
+};
