da8bec78e20d35748a0fb265abb120f485efce74
App Academy Student <ryandgoldenberg1@gmail.com>
2015-12-11 22:36:20 -0500

    Make constants public.

--- app/assets/javascripts/utils/infinite_scroll.js
+++ app/assets/javascripts/utils/infinite_scroll.js
@@ -1,36 +1,36 @@
 var InfiniteScroll = {
-  _SCROLL_RATIO: 0.75,
-  _CONTAINER: window,
-  _CONTENT: document,
+  SCROLL_RATIO: 0.75,
+  CONTAINER: window,
+  CONTENT: document,
 
   componentDidMount: function() {
     this._fetchInitialData();
-    $(this._CONTAINER).on("scroll", this._onScroll);
+    $(this.CONTAINER).on("scroll", this._onScroll);
   },
 
   componentWillUnmount: function() {
-    $(this._CONTAINER).off("scroll", this._onScroll);
+    $(this.CONTAINER).off("scroll", this._onScroll);
   },
 
   _onScroll: function(e) {
-    var maxScroll = $(this._CONTENT).height() - $(this._CONTAINER).height();
-    var percentage = this._CONTAINER.scrollY / maxScroll;
+    var maxScroll = $(this.CONTENT).height() - $(this.CONTAINER).height();
+    var percentage = this.CONTAINER.scrollY / maxScroll;
 
-    if (percentage >= this._SCROLL_RATIO)
+    if (percentage >= this.SCROLL_RATIO)
       this._fetchMoreData();
   },
 
   _fetchInitialData: function() {
     if (typeof this.fetchInitialData !== "function")
-      throw "InfiniteScroll requires fetchInitialData()";
+      throw "Must implement fetchInitialData()";
 
     this._startFetching();
     this.fetchInitialData().then(this._finishFetching);
   },
 
   _fetchMoreData: function() {
-    if (typeof this.fetchMoreData !== "function")
-      throw "InfiniteScroll requires fetchMoreData()";
+    if (typeof this.fetchMoreData !== "function") 
+      throw "Must implement fetchMoreData()";
 
     if (this._isFetching) return;
     this._startFetching();
