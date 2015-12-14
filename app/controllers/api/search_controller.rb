2269ceb0aba5ceaf82e05ad9a8c4b8fc9923f9bf
App Academy Student <ryandgoldenberg1@gmail.com>
2015-12-14 16:30:45 -0500

    Include nicknames in search results.

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,12 +1,12 @@
 class Api::SearchController < ApplicationController
 
   def user_search
-    @users = User.search_by_full_name(params[:name]).includes(:avatar)
+    @users = User.search_by_full_name(params[:name]).includes(:avatar, :nicknames)
     render :users
   end
 
   def user_search_preview
-    @users = User.search_by_full_name(params[:name]).includes(:avatar).limit(10)
+    @users = User.search_by_full_name(params[:name]).includes(:avatar, :nicknames).limit(10)
     render :users
   end
 
--- app/views/api/search/users.json.jbuilder
+++ app/views/api/search/users.json.jbuilder
@@ -1,3 +1,6 @@
 json.array! @users do |user|
   json.partial! 'api/users/user', user: user
+  json.nicknames do
+    json.array! user.nicknames
+  end
 end
