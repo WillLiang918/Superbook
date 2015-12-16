f6e1c730ebef251e7c2b632311e531b109538f16
App Academy Student <ryandgoldenberg1@gmail.com>
2015-12-16 17:44:53 -0500

    Create Messages component and MessageLayout.

---
+++ app/assets/javascripts/components/layout/message_layout.js.jsx
@@ -0,0 +1,13 @@
+var MessageLayout = React.createClass({
+  render: function() {
+    return (
+      <div className="main-container flex-container">
+        <nav className="side-nav"></nav>
+        <section className="feed">
+          
+        </section>
+        <aside className="aside"></aside>
+      </div>
+    );
+  }
+});
