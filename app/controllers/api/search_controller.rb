fc920a5cf3cc4be78c2e509bea9cf9210b2b498e
App Academy Student <ryandgoldenberg1@gmail.com>
2015-12-16 08:47:22 -0500

    Fix search bug with old avatar.

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,12 +1,12 @@
 class Api::SearchController < ApplicationController
 
   def user_search
-    @users = User.search_by_full_name(params[:name]).includes(:avatar, :nicknames)
+    @users = User.search_by_full_name(params[:name]).includes(:nicknames)
     render :users
   end
 
   def user_search_preview
-    @users = User.search_by_full_name(params[:name]).includes(:avatar, :nicknames).limit(10)
+    @users = User.search_by_full_name(params[:name]).includes(:nicknames).limit(10)
     render :users
   end
 
